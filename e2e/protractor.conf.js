a370e524246d855caef3d5908de766e205c462bf
Marco De la Cruz <antonio.123147@hotmail.com>
2020-11-08 00:26:50 -0500

    test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -7,26 +7,47 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 /**
  * @type { import("protractor").Config }
  */
+
 exports.config = {
-  allScriptsTimeout: 11000,
+  // set to "custom" instead of cucumber.
+  framework: 'custom',
+
+  // path relative to the current config file
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+
+  // require feature files
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './src/specs/*.feature' // accepts a glob
   ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.json')
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  baseUrl: 'http://localhost:4200/auth',
+  // seleniumAddress: 'http://localhost:4444/wd/hub/',
+  cucumberOpts: {
+    // require step definitions
+    require: [
+      './src/steps/*.steps.ts' // accepts a glob
+    ]
   }
 };
+// exports.config = {
+//   allScriptsTimeout: 11000,
+//   specs: [
+//     './src/**/*.e2e-spec.ts'
+//   ],
+//   capabilities: {
+//     'browserName': 'chrome'
+//   },
+//   directConnect: true,
+//   baseUrl: 'http://localhost:4200/',
+//   framework: 'jasmine',
+//   jasmineNodeOpts: {
+//     showColors: true,
+//     defaultTimeoutInterval: 30000,
+//     print: function() {}
+//   },
+//   onPrepare() {
+//     require('ts-node').register({
+//       project: require('path').join(__dirname, './tsconfig.json')
+//     });
+//     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+//   }
+// };
